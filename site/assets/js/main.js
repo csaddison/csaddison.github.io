5de54e07faecb322fc75ca04f3d0548d20d766bf
Conner Addison <47577993+csaddison@users.noreply.github.com>
2021-03-05 09:32:18 -0800

    Adding new test site in /site folder

---
+++ site/assets/js/main.js
@@ -0,0 +1,25 @@
+// function setup() {
+
+// 	var clientHeight = document.getElementById('sketch').clientHeight;
+// 	var clientWidth = document.getElementById('sketch').clientWidth;
+
+// 	var cnv = createCanvas(clientWidth, clientHeight);
+// 	cnv.parent("sketch");
+// 	background(0);
+// }
+
+// function draw() {
+//     background(0);
+//     circle(mouseX, mouseY, 15)
+// }
+
+// <div id="sketch"></div>
+
+// #sketch {
+//     width: 500px;
+//     height: 500px;
+// }
+
+// canvas {
+//     position: relative;
+// }
